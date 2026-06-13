9774215c2ed5f5769ed395c276a870054af7c3e4
exxesa <notmajorka@gmail.com>
2026-06-13 20:54:49 +0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 
 const baseRoutes = require("./routes/base");
 
@@ -10,16 +11,27 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use(
+    express.static(
+        path.join(__dirname, "../frontend")
+    )
+);
+
 app.use("/api/base", baseRoutes);
 
 app.get("/", (req, res) => {
-res.json({
-message: "GB Map API is running"
-});
+
+    res.sendFile(
+        path.join(
+            __dirname,
+            "../frontend/index.html"
+        )
+    );
+
 });
 
 const PORT = process.env.PORT || 10000;
 
 app.listen(PORT, () => {
-console.log(`Server started on port ${PORT}`);
+    console.log(`Server started on port ${PORT}`);
 });
