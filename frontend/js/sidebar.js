e4c85836d18a013b2f3e3fd5154a42954482ba7a
exxesa <notmajorka@gmail.com>
2026-06-13 20:46:00 +0500

    Update sidebar.js

--- frontend/js/sidebar.js
+++ frontend/js/sidebar.js
@@ -1,12 +1,84 @@
-function showAddForm(){
+/*
+==========================================
+FORM ELEMENTS
+==========================================
+*/
 
-const name =
-prompt("Market name");
+const addButton =
+    document.getElementById("addButton");
 
-if(!name) return;
+const saveButton =
+    document.getElementById("saveButton");
 
-alert(
-"Form will be implemented later"
-);
+const nameInput =
+    document.getElementById("name");
+
+const countryInput =
+    document.getElementById("country");
+
+const cityInput =
+    document.getElementById("city");
+
+const categoryInput =
+    document.getElementById("category");
+
+const descriptionInput =
+    document.getElementById("description");
+
+/*
+==========================================
+ADD NEW OBJECT
+==========================================
+*/
+
+async function saveBaseObject() {
+
+    if (!selectedCoordinates) {
+
+        alert(
+            "Select a point on the map first"
+        );
+
+        return;
+    }
+
+    const baseData = {
+
+        name: nameInput.value,
+
+        country: countryInput.value,
+
+        city: cityInput.value,
+
+        category: categoryInput.value,
+
+        coordinates: selectedCoordinates,
+
+        description:
+            descriptionInput.value
+
+    };
+
+    const result =
+        await addBaseData(baseData);
+
+    console.log(result);
+
+    location.reload();
+
+}
+
+/*
+==========================================
+SAVE BUTTON EVENT
+==========================================
+*/
+
+if (saveButton) {
+
+    saveButton.addEventListener(
+        "click",
+        saveBaseObject
+    );
 
 }
