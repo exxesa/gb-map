f054964e17230f23abc66d85208e5195b2c05a28
exxesa <notmajorka@gmail.com>
2026-06-14 13:37:17 +0500

    Update map.js

--- frontend/js/map.js
+++ frontend/js/map.js
@@ -24,7 +24,7 @@ const map =
     .setView([20, 0], 2);
 
 L.tileLayer(
-    "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+    "https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png",
     {
         maxZoom: 19,
         attribution:
